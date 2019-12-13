1cc58e9c5044fec68a17749b208eee649a6c83ef
TimDepping <tim-lorenz@depping.org>
2019-12-13 14:01:11 +0100

    initial commit

---
+++ Uebung1/main.c
@@ -0,0 +1,14 @@
+/*
+Uebung:				1
+Autoren:				Tim Depping
+Matrikelnummer:		20206723
+Studiengang:			Informatik
+*/
+
+#include "functions.h"
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+	printf("Hello World");
+	return 0;
+}
